6fda87f5d8586770d755c799051af81a0e388b0a
Klarkkent3 <115928015+Klarkkent3@users.noreply.github.com>
2022-10-27 22:53:27 +0300

    Create 6.4_Step2.go

---
+++ 6.4/6.4_Step2.go
@@ -0,0 +1,9 @@
+/*
+Дополните (замените нижние подчеркивания) код, чтобы получилась правильная карта countries.
+*/
+
+countries := map [string]string {
+    "us": "USA",
+    "fr": "France",
+    "ch": "China"}
+
