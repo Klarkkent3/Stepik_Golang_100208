8b739d8b2e0bc801276d0febc52c03538e7bd6e2
Klarkkent3 <115928015+Klarkkent3@users.noreply.github.com>
2022-10-27 22:17:03 +0300

    Create 3.10_Step4.go

---
+++ 3.10/3.10_Step4.go
@@ -0,0 +1,16 @@
+/*
+Давайте исправим еще одну программу. Нам нужно, чтобы получился правильный оператор if
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    height := 195
+    if height > 185 {
+        fmt.Println("Да")
+    } else {
+        fmt.Println("Нет")
+    }
+}
