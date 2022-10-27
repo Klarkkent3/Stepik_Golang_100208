efca9c067a68e1183fb88604350e65f57f8c0272
Klarkkent3 <115928015+Klarkkent3@users.noreply.github.com>
2022-10-27 22:28:14 +0300

    Create 4.2_Step4.go

---
+++ 4.2/4.2_Step4.go
@@ -0,0 +1,15 @@
+/*
+Дополните (замените нижние подчеркивания) код, чтобы определить функцию mult, которая принимает два целочисленных аргумента и выводит результат их произведения.
+
+Sample Input:
+5 6
+
+Sample Output:
+30
+*/
+
+//For this tasks, we need to write only function, package "fmt" and func main () already declared.
+
+func mult(x, y int) {
+    fmt.Println(x*y)
+}
