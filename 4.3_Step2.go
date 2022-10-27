2fb26032ba2e47c1d1935dd11cfca084f71f3299
Klarkkent3 <115928015+Klarkkent3@users.noreply.github.com>
2022-10-27 22:29:33 +0300

    Create 4.3_Step2.go

---
+++ 4.3_Step2.go
@@ -0,0 +1,16 @@
+/*
+Дополните (замените нижние подчеркивания) код, чтобы определить функцию, которая принимает две строки в качестве аргументов 
+и возвращает результат их объединения (конкатенации).
+
+Sample Input:
+concat enation
+
+Sample Output:
+concatenation
+*/
+
+//For this tasks, we need to write only function, package "fmt" and func main () already declared.
+
+func concat(a, b string) string {
+    return a + b
+}
